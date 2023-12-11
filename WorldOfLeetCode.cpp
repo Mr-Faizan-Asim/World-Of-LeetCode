416b11f6b8301e2817e0cda6104d3c24011d54b6
Muhammad Faizan Asim <120254196+Mr-Faizan-Asim@users.noreply.github.com>
2023-12-12 03:16:31 +0500

    Update WorldOfLeetCode.cpp

--- WorldOfLeetCode.cpp
+++ WorldOfLeetCode.cpp
@@ -12,3 +12,21 @@ public:
         return -1;
     }
 };
+
+//441. Arranging Coins
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+     int c = n;
+     int i = 1;
+     while(c > 0)
+     {
+         if(n == 0) return 0;
+         else if(n == 1) return 1;
+        i++;
+        c-=i;
+     }
+     return i -1;
+    }
+};
