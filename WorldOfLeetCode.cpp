1d12b19e421d5bac3b73d2e86e48fb4e0a071660
Muhammad Faizan Asim <120254196+Mr-Faizan-Asim@users.noreply.github.com>
2023-12-13 02:20:40 +0500

    Update WorldOfLeetCode.cpp

--- WorldOfLeetCode.cpp
+++ WorldOfLeetCode.cpp
@@ -30,3 +30,22 @@ public:
      return i -1;
     }
 };
+//991. Broken Calculator
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int result = 0;
+        while(target > startValue)
+        {
+            result++;
+            if(target%2 == 0)
+        {
+            target = target/2;
+        }
+        else
+        target+=1;
+        }
+        result = result + (startValue - target);
+        return result;
+    }
+};
